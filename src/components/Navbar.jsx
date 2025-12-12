9c64d4df5253bc75bf848b397e16db7bba62890d
Alexis Manosca <alexismanosca@gmail.com>
2025-12-12 14:12:42 +1300

    added the foundation of the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,10 @@ export default function Navbar() {
 
   return (
     <nav>
-		<h1>navbar here</h1>
+        <button style={{'zIndex': 4}}>Home</button>
+        <button style={{'zIndex': 3}}>About Me</button>
+        <button style={{'zIndex': 2}}>Projects</button>
+        <button style={{'zIndex': 1}}>Contact</button>
 	</nav>
   )
 }
