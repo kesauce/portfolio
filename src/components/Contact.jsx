54c5792b8d8fe99ac83ab9981368f8f4adb4e4e9
Alexis Manosca <alexismanosca@gmail.com>
2025-12-17 22:55:30 +1300

    added a contact form that's connected to a third party tool that won't require me to put my email onto the website

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,59 @@
-export default function Contact() {
+import { useForm, ValidationError } from '@formspree/react';
+import '../styles/Contact.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-  return (
-    <section className='main-content'>
-        <h1>this is the contact</h1>
-    </section>
-  )
+export default function Contact() {
+	const [state, handleSubmit] = useForm("xgvggvpl");
+
+  	return (
+		<section className='main-content contact'>
+			<h1>Get In Touch</h1>
+			<h6>Open to internships and roles!</h6>
+			<form className='contact-form' onSubmit={handleSubmit}>
+				<h3><FontAwesomeIcon icon={faEnvelope} />Email Me</h3>
+				<input
+					id="name"
+					type="text" 
+					name="name"
+					placeholder='Name'
+				/>
+				<ValidationError 
+					prefix="Name" 
+					field="name"
+					errors={state.errors}
+				/>
+				<input
+					id="email"
+					type="email" 
+					name="email"
+					placeholder='Email'
+				/>
+				<ValidationError 
+					prefix="Email" 
+					field="email"
+					errors={state.errors}
+				/>
+				<textarea
+					id="message"
+					name="message"
+					placeholder='Message'
+				/>
+				<ValidationError 
+					prefix="Message" 
+					field="message"
+					errors={state.errors}
+				/>
+				<button className='button-45'type="submit" disabled={state.submitting}>Submit</button>
+			</form>
+			{
+				state.succeeded ? <h3>Thanks! I'll get back to you as soon as I can.</h3> : undefined
+			}
+			<div className='links'>
+				<a className='social-link' href='https://github.com/kesauce' target="_blank" rel="noopener noreferrer"><h3><FontAwesomeIcon icon={faGithub} /> Github</h3></a>
+				<a className='social-link' href='https://www.linkedin.com/in/alexis-ma%C3%B1osca-7b015a354/' target="_blank" rel="noopener noreferrer"><h3><FontAwesomeIcon icon={faLinkedin} />LinkedIn</h3></a>
+			</div>
+		</section>
+	)
 }
