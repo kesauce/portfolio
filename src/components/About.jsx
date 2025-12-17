4ae3eb183ae95cada334bd63365a65055bdb4419
Alexis Manosca <alexismanosca@gmail.com>
2025-12-18 11:50:54 +1300

    started my about me message

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,19 +4,19 @@ import '../styles/About.css';
 
 export default function About() {
 
-  return (
-    <section className='main-content about'>
-        <div>
-			<h1>Why I'm Here</h1>
-			<p className='work-sans-p'>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, aliquid eveniet? Rerum hic qui sunt reiciendis quidem quaerat sequi, veritatis consectetur laboriosam assumenda eveniet et aspernatur similique a consequatur cumque.
-			</p>
-		</div>
-		<hr style={{borderTop: '2px solid var(--crimson)', width: '80%', color:'var(--crimson)'}}/>
-		<div className='list-sections'>
-			<Languages />
-			<Skills />
-		</div>
-    </section>
-  )
+	return (
+		<section className='main-content about'>
+			<div>
+				<h1>Why I'm Here</h1>
+				<p className='work-sans-p'>
+					Ever since I was young, I've always been interested in computers. I've always loved how you can discover, learn and do so many things over the internet. Now that I've learnt more, I realised that there are so many ways to improve one's experience. Privacy, safety, accessibility.
+				</p>
+			</div>
+			<hr style={{ borderTop: '2px solid var(--crimson)', width: '80%', color: 'var(--crimson)' }} />
+			<div className='list-sections'>
+				<Languages />
+				<Skills />
+			</div>
+		</section>
+	)
 }
